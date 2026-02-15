ffc99a557e00b14e35149b43cc9d7d18a919912e
AntonioRafaelFonseca <rafamarim19@gmail.com>
2026-02-15 20:19:47 +0000

    first commit!!!

---
+++ main.js
@@ -0,0 +1,44 @@
+if(window.location.href.includes('slide'))
+{
+  let current = (() => {
+    const match = window.location.href.match(/slide(\d+)\.html$/);
+    return match ? Number(match[1]) : 0;
+  })();
+
+  const maxSlides = 4; // Set this to your total number of slides
+
+  document.addEventListener("keydown", (k) => {
+
+    if (k.key === "ArrowRight") {
+      if (current < maxSlides) {
+        current++;
+        window.location.href = `slide${current}.html`;
+      }else
+      {
+      window.location.href = 'referencias.html'
+      }
+      
+    }
+
+    else if (k.key === "ArrowLeft") {
+      if (current == 0) {
+        window.location.href = 'index.html'
+      }
+      else
+      {
+        current--;
+        window.location.href = `slide${current}.html`;
+      }
+      
+    }
+  });
+}
+else
+{
+  document.addEventListener('keydown', e => {
+    if (e.key == 'ArrowLeft')
+    {
+      window.location.href = 'slide4.html'
+    }
+  });
+}
